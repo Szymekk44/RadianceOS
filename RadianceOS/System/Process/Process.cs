9eb43720bb50a2eb3896c4263c28ff834cade1cb
MrBisquit <william@wtdawson.info>
2024-01-25 20:17:53 +0000

    Begun work on the new process manager

---
+++ RadianceOS/System/Process/Process.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadianceOS.System.Process
+{
+    public class Process
+    {
+        public string ApplicationName { get; private set; } = null;
+        public string Title { get; set; } = null;
+
+        public Action Update = null;
+        public Security.ProcessSecurityInfo SecurityInfo { get; private set; }
+
+        public ProcessType CurrentProcessType()
+        {
+            if (GetType() == typeof(Process)) return ProcessType.Process;
+            if (GetType() == typeof(Service)) return ProcessType.Service;
+            if (GetType() == typeof(Application)) return ProcessType.Application;
+            else return default;
+        }
+    }
+
+    public class Service : Process
+    {
+
+    }
+
+    public class Application : Process
+    {
+
+    }
+
+    public enum ProcessType
+    {
+        Process = 0,
+        Service = 1,
+        Application = 2
+    }
+}
---
+++ RadianceOS/System/Process/ProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadianceOS.System.Process
+{
+    public static class ProcessManager
+    {
+        public static List<Process> Processes { get; private set; } = new List<Process>();
+
+        public static void Update()
+        {
+            foreach (Process process in Processes)
+            {
+                if (process.Update != null) process.Update();
+                else throw new InvalidProcessException(InvalidProcessException.Type.InvalidUpdateAction);
+            }
+        }
+
+        public static void RegisterProcess(Process process)
+        {
+            if(process.CurrentProcessType() == ProcessType.Process)
+            {
+                throw new InvalidProcessException(InvalidProcessException.Type.InvalidProcessType);
+            }
+        }
+
+        public class InvalidProcessException : Exception
+        {
+            public enum Type
+            {
+                InvalidProcessType = 0,
+                InvalidUpdateAction = 1,
+            }
+
+            public Type ExceptionType;
+
+            public InvalidProcessException(Type ExceptionType)
+            {
+                this.ExceptionType = ExceptionType;
+            }
+        }
+    }
+}
